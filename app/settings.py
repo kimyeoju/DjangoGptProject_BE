20ac650a3e3b9e73f2ad3227c809de0ce34e9058
kimyeoju <yeojoo0031@gmail.com>
2024-02-19 19:04:33 +0900

    chore : 환경세팅 수정

--- app/settings.py
+++ app/settings.py
@@ -1,6 +1,12 @@
 from pathlib import Path
-import dotenv
 import os
+from dotenv import load_dotenv
+
+load_dotenv(os.path.join(BASE_DIR, '.env'))
+
+APIKEY = os.getenv('OPENAI_API_KEY')
+
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
